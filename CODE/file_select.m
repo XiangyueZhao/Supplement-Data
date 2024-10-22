4a3496bd1e66f78c749b3153c7a4fa0f8b84b822
XiangyueZhao <326910159@qq.com>
2024-10-22 23:08:15 +0800

    version 1.1

--- CODE/file_select.m
+++ CODE/file_select.m
@@ -19,12 +19,12 @@ powerall=[2.5 2.75 3 3.25 3.5 3.75 4 4.25 4.5];
 for k=1:length(powerall)
     power=powerall(k);
     powername=num2str(power);
-    save_path = ['G:\bids_ALL_result\power\' powername];% change your path
+    save_path = ['G:\BIDS\ERP\ANT\POWER\' powername];% change your path
 %     t=[];
 %     for subi = 1:26
 %         behav_data = tdfread('G:\bids_ALL_result\BIDS_26_ByBiosemi\participants.tsv');% change your path
 %         sub = behav_data.participant_id;
-%         input_path = 'G:\bids_ALL_result\derivatives\26\fz\';% change your path
+%         input_path = 'G:\bids_ALL_result\derivatives\26\rerefer\';% change your path
 %         file_name = [sub(subi, :) '_26ByBiosemi.set'];
 %         power_screen (file_name,input_path,save_path,power,t);% The function requires an initial array as input
 %     end
@@ -38,11 +38,11 @@ for k=1:length(powerall)
 %     end
     t=[];
     for subi = 1:29
-        behav_data = tdfread('G:\bids_ALL_result\BIDS_29_ByBP\participants.tsv');% change your path
+        behav_data = tdfread('G:\BIDS_bigdata\BIDS_29_ByANT\participants.tsv');% change your path
         sub = behav_data.participant_id;
-        input_path = 'G:\bids_ALL_result\BIDS_29_ByBP\derivates\fz\';% change your path
+        input_path = 'G:\BIDS_bigdata\BIDS_29_ByANT\derivatives\rerefer\';% change your path
         file_name = [sub(subi, :) '_29ByANT.set'];
-        power_screen (file_name,input_path,save_path,power,t);
+        rating_screen (file_name,input_path,save_path,power,t);
     end
 %     t=[];
 %     for subi = 1:30
@@ -54,7 +54,6 @@ for k=1:length(powerall)
 %     end
 %     t=[];
 %     for subi = 1:39
-%         
 %         behav_data = tdfread('G:\bids_ALL_result\BIDS_39_ByBP\participants.tsv');% change your path
 %         sub = behav_data.participant_id;
 %         input_path = 'G:\bids_ALL_result\derivatives\39\fz\';% change your path
@@ -63,7 +62,6 @@ for k=1:length(powerall)
 %     end
 %     t=[];
 %     for subi = 1:65
-%         
 %         behav_data = tdfread('G:\bids_ALL_result\BIDS_65_ByANT\participants.tsv');% change your path
 %         sub = behav_data.participant_id;
 %         input_path = 'G:\bids_ALL_result\BIDS_65_ByANT\derivatives\rerefer\';% change your path
@@ -86,7 +84,6 @@ for k=1:length(powerall)
 %         file_name = [sub(subi, :) '_223ByBP.set'];
 %         power_screen (file_name,input_path,save_path,power,t);
 %     end
-%     
 %     t=[];
 %     for subi = 1:95
 %         behav_data = tdfread('G:\bids_ALL_result\BIDS_95_ByBP\participants.tsv');% change your path
@@ -106,14 +103,14 @@ for k=1:length(ratingall)
     rating=rating(k);
     ratingname=num2str(rating);
     save_path = ['G:\bids_ALL_result\fzrating\' ratingname];% change your path
-    t=[];
-    for subi = 1:26
-        behav_data = tdfread('G:\bids_ALL_result\BIDS_26_ByBiosemi\participants.tsv');% change your path
-        sub = behav_data.participant_id;
-        input_path = 'G:\bids_ALL_result\derivatives\26\fz\';% change your path
-        file_name = [sub(subi, :) '_26ByBiosemi.set'];
-        rating_screen (file_name,input_path,save_path,rating,t);
-    end
+%     t=[];
+%     for subi = 1:26
+%         behav_data = tdfread('G:\bids_ALL_result\BIDS_26_ByBiosemi\participants.tsv');% change your path
+%         sub = behav_data.participant_id;
+%         input_path = 'G:\bids_ALL_result\derivatives\26\fz\';% change your path
+%         file_name = [sub(subi, :) '_26ByBiosemi.set'];
+%         rating_screen (file_name,input_path,save_path,rating,t);
+%     end
     t=[];
     for subi = 1:29
         behav_data = tdfread('G:\bids_ALL_result\BIDS_29_ByANT\participants.tsv');% change your path
--- CODE/mat_creation.m
+++ CODE/mat_creation.m
@@ -6,41 +6,47 @@ The mat_generate function is located in the code-function folder.
 %}
 %% power
 clear;clc;
-powerall=[2.75 3 3.25 3.5 3.75 4 4.25 4.5];
+powerall=[2.5 2.75 3 3.25 3.5 3.75 4 4.25 4.5];
 for k=1:length(powerall)
-    power=powerall(k);
-    powername=num2str(power);
-    file_name = ['G:\bids_ALL_result\fzpower\' powername '.mat'];% change your path
-    file_path = ['G:\bids_ALL_result\fzpower\' powername];% change your path
-    A26 = dir(fullfile(file_path, '*26ByBiosemi*set*')); % Acpuire all designated files
-    B26 = {A26.name};
-    A29 = dir(fullfile(file_path, '*29ByANT*set*'));
-    B29 = {A29.name};
-    C29 = dir(fullfile(file_path, '*29ByBP*set*'));
-    D29 = {C29.name};
-    A30 = dir(fullfile(file_path, '*30ByANT*set*')); 
-    B30 = {A30.name};
-    A39 = dir(fullfile(file_path, '*39ByBP*set*')); 
-    B39 = {A39.name};
-    A65 = dir(fullfile(file_path, '*65ByANT*set*')); 
-    B65 = {A65.name};
-    A142 = dir(fullfile(file_path, '*142ByBiosemi*set*')); 
-    B142 = {A142.name};
-    A223= dir(fullfile(file_path, '*223ByBP*set*')); 
-    B223 = {A223.name};
-    A95 = dir(fullfile(file_path, '*95ByBP*set*')); 
-    B95 = {A95.name};
-    eeglab
-    chanlable = "Cz";% the electrodes intend for extraction
-    cz1 = mat_generate (B26,file_path,chanlable);
-    cz2 = mat_generate (B29,file_path,chanlable);
-    cz3 = mat_generate (B30,file_path,chanlable);
-    cz4 = mat_generate (B39,file_path,chanlable);
-    cz5 = mat_generate (B65,file_path,chanlable);
-    cz6 = mat_generate (B142,file_path,chanlable);
-    cz7 = mat_generate (B223,file_path,chanlable);
-    cz8 = mat_generate (B95,file_path,chanlable);
-    cz9 = mat_generate (D29,file_path,chanlable);
+power=powerall(k);
+powername=num2str(power);
+file_path1 = ['H:\BIDS\ERP\ANT\POWER\' powername];% change your path
+file_path2 = ['H:\BIDS\ERP\BioSemi\POWER\' powername];% change your path
+file_path3 = ['H:\BIDS\ERP\BP\POWER\' powername];% change your path
+A26 = dir(fullfile(file_path2, '*26ByBiosemi*set*')); % Acpuire all designated files
+B26 = {A26.name};
+A29 = dir(fullfile(file_path1, '*29ByANT*set*'));
+B29 = {A29.name};
+C29 = dir(fullfile(file_path3, '*29ByBP*set*'));
+D29 = {C29.name};
+A30 = dir(fullfile(file_path1, '*30ByANT*set*')); 
+B30 = {A30.name};
+A39 = dir(fullfile(file_path3, '*39ByBP*set*')); 
+B39 = {A39.name};
+A65 = dir(fullfile(file_path1, '*65ByANT*set*')); 
+B65 = {A65.name};
+A142 = dir(fullfile(file_path2, '*142ByBiosemi*set*')); 
+B142 = {A142.name};
+A223= dir(fullfile(file_path3, '*223ByBP*set*')); 
+B223 = {A223.name};
+A95 = dir(fullfile(file_path3, '*95ByBP*set*')); 
+B95 = {A95.name};
+eeglab;
+
+chanlable1 = ["FP1","FPZ","FP2","F7","F3","FZ","F4","F8","FC5","FC1","FC2","FC6","T7","C3","CZ","C4","T8","CP5","CP1","CP2","CP6","P7","P3","PZ","P4","P8","POZ","O1","OZ","O2"];
+chanlable2 = ["Fp1","Fpz","Fp2","F7","F3","Fz","F4","F8","FC5","FC1","FC2","FC6","T7","C3","Cz","C4","T8","CP5","CP1","CP2","CP6","P7","P3","Pz","P4","P8","POz","O1","Oz","O2"];
+
+for i=1:30
+channame=num2str(i);
+cz1 = mat_generate2 (B26,file_path2,chanlable1(i));
+cz2 = mat_generate2 (B29,file_path1,chanlable2(i));
+    cz3 = mat_generate2 (B30,file_path1,chanlable2(i));
+    cz4 = mat_generate2 (B39,file_path3,chanlable2(i));
+    cz5 = mat_generate2 (B65,file_path1,chanlable2(i));
+    cz6 = mat_generate2 (B142,file_path2,chanlable1(i));
+    cz7 = mat_generate2 (B223,file_path3,chanlable2(i));
+    cz8 = mat_generate2 (B95,file_path3,chanlable2(i));
+    cz9 = mat_generate2 (D29,file_path3,chanlable2(i));
     % synthetic array and save
     dataselect={};
     for d=1:9
@@ -49,10 +55,12 @@ for k=1:length(powerall)
             dataselect{end+1}=eval(varname);
         end
     end
-    data=cat(1,dataselect{:});
-    tepoch=EEG.times;
-    EEG=[];EEG.times=tepoch; 
-    save( file_name ,'data','EEG');
+data=cat(1,dataselect{:});
+tepoch=EEG.times;
+EEG=[];EEG.times=tepoch; 
+file_name = ['H:\BIDS\powerall\' powername '\' channame '.mat'];% change your path
+save( file_name ,'data','EEG');
+end
 end
 
 %% rating
@@ -61,60 +69,78 @@ ratingall=1:10;
 for k=1:length(ratingall)
     rating=ratingall(k);
     ratingname=num2str(rating);
-    file_name = ['G:\bids_ALL_result\fzrating\' ratingname '.mat'];% change your path
-    file_path = ['G:\bids_ALL_result\fzrating\' ratingname];% change your path
-     A26 = dir(fullfile(file_path, '*26ByBiosemi*set*')); % Acpuire all designated files
+    file_path1 = ['H:\BIDS\ERP\ANT\RATING\' ratingname];% change your path
+    file_path2 = ['H:\BIDS\ERP\BioSemi\RATING\' ratingname];% change your path
+    file_path3 = ['H:\BIDS\ERP\BP\RATING\' ratingname];% change your path
+    A26 = dir(fullfile(file_path2, '*26ByBiosemi*set*')); % Acpuire all designated files
     B26 = {A26.name};
-    A29 = dir(fullfile(file_path, '*29ByANT*set*'));
+    A29 = dir(fullfile(file_path1, '*29ByANT*set*'));
     B29 = {A29.name};
-    C29 = dir(fullfile(file_path, '*29ByBP*set*'));
+    C29 = dir(fullfile(file_path3, '*29ByBP*set*'));
     D29 = {C29.name};
-    A30 = dir(fullfile(file_path, '*30ByANT*set*')); 
+    A30 = dir(fullfile(file_path1, '*30ByANT*set*')); 
     B30 = {A30.name};
-    A39 = dir(fullfile(file_path, '*39ByBP*set*')); 
+    A39 = dir(fullfile(file_path3, '*39ByBP*set*')); 
     B39 = {A39.name};
-    A65 = dir(fullfile(file_path, '*65ByANT*set*')); 
+    A65 = dir(fullfile(file_path1, '*65ByANT*set*')); 
     B65 = {A65.name};
-    A142 = dir(fullfile(file_path, '*142ByBiosemi*set*')); 
+    A142 = dir(fullfile(file_path2, '*142ByBiosemi*set*')); 
     B142 = {A142.name};
-    A223= dir(fullfile(file_path, '*223ByBP*set*')); 
+    A223= dir(fullfile(file_path3, '*223ByBP*set*')); 
     B223 = {A223.name};
-    A95 = dir(fullfile(file_path, '*95ByBP*set*')); 
+    A95 = dir(fullfile(file_path3, '*95ByBP*set*')); 
     B95 = {A95.name};
     eeglab
-    chanlable = "Cz";% the electrodes intend for extraction
-    cz1 = mat_generate (B26,file_path,chanlable);
-    cz2 = mat_generate (B29,file_path,chanlable);
-    cz3 = mat_generate (B30,file_path,chanlable);
-    cz4 = mat_generate (B39,file_path,chanlable);
-    cz5 = mat_generate (B65,file_path,chanlable);
-    cz6 = mat_generate (B142,file_path,chanlable);
-    cz7 = mat_generate (B223,file_path,chanlable);
-    cz8 = mat_generate (B95,file_path,chanlable);
-    cz9 = mat_generate (D29,file_path,chanlable);
-    % synthetic array and save
-    dataselect={};
-    for d=1:9
-        varname=sprintf('cz%d',d);
-        if exist(varname,'var')
-            dataselect{end+1}=eval(varname);
+     chanlable1 = ["FP1","FPZ","FP2","F7","F3","FZ","F4","F8","FC5","FC1","FC2","FC6","T7","C3","CZ","C4","T8","CP5","CP1","CP2","CP6","P7","P3","PZ","P4","P8","POZ","O1","OZ","O2"];
+chanlable2 = ["Fp1","Fpz","Fp2","F7","F3","Fz","F4","F8","FC5","FC1","FC2","FC6","T7","C3","Cz","C4","T8","CP5","CP1","CP2","CP6","P7","P3","Pz","P4","P8","POz","O1","Oz","O2"];
+    for i=1:30
+        channame=num2str(i);
+        cz1 = mat_generate2 (B26,file_path2,chanlable1(i));
+        cz2 = mat_generate2 (B29,file_path1,chanlable2(i));
+        cz3 = mat_generate2 (B30,file_path1,chanlable2(i));
+        cz4 = mat_generate2 (B39,file_path3,chanlable2(i));
+        cz5 = mat_generate2 (B65,file_path1,chanlable2(i));
+        cz6 = mat_generate2 (B142,file_path2,chanlable1(i));
+        cz7 = mat_generate2 (B223,file_path3,chanlable2(i));
+        cz8 = mat_generate2 (B95,file_path3,chanlable2(i));
+        cz9 = mat_generate2 (D29,file_path3,chanlable2(i));
+        % synthetic array and save
+        dataselect={};
+        for d=1:9
+            varname=sprintf('cz%d',d);
+            if exist(varname,'var')
+                dataselect{end+1}=eval(varname);
+            end
         end
-    end
     data=cat(1,dataselect{:});
     tepoch=EEG.times;
     EEG=[];EEG.times=tepoch; 
+    file_name = ['H:\BIDS\ratingall\' ratingname '\' channame '.mat'];% change your path
     save( file_name ,'data','EEG');
+    end
 end
+%% create an array of all intensities or ratings if needed
+powerall=[2.5 2.75 3 3.25 3.5 3.75 4 4.25 4.5];
+n={};
+for i=1:length(powerall)
+    power=powerall(i);
+    powername=num2str(power);
+    file_name = ['H:\BIDS\powerall\' powername '\15.mat'];% change your path
+    load(file_name);
+    n{i}=data(:,:,:);
+end
+save('H:\BIDS\ERP-power.mat','n','EEG')% change your path
 
 %% create an array of all intensities or ratings if needed
-% ratingall=[2.5 2.75 3 3.25 3.5 3.75 4 4.25 4.5];
-rating=1:10;
+ratingall=1:10;
 n={};
 for i=1:length(ratingall)
     rating=ratingall(i);
     ratingname=num2str(rating);
-    file_name = ['G:\bids_ALL_result\rating\' ratingname '.mat'];% change your path
+    file_name = ['H:\BIDS\ratingall\' ratingname '\15.mat'];% change your path
     load(file_name);
-    n{i}=data(:,:,:);
+    n{i}=data;
 end
-save('G:\bids_ALL_result\rating\all.mat','n','EEG')% change your path
+ save('H:\BIDS\ERP-rating.mat','n','EEG')% change your path
+
+
